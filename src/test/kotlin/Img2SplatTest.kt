e91f0e75f5c9af132939054c6dae23159230c001
Jonathan Nye <jonba2@gmail.com>
2022-10-08 15:53:23 -0400

    Fix test to account for row extent optimization

--- src/test/kotlin/Img2SplatTest.kt
+++ src/test/kotlin/Img2SplatTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 class Img2SplatTest {
 
     @Test
-    fun `Full preview image matches monochrome input image`() {
+    fun `Full preview image matches expected preview`() {
         val testFilePath = this::class.java.getResource("test_jiji.png").file
         val options = Img2Splat.Options.validateAndBuildFromInput(
             filePath = testFilePath,
@@ -15,17 +15,17 @@ class Img2SplatTest {
         )
         val result = Img2Splat(options).splat()
 
-        val inputImg = ImageIO.read(File(testFilePath))
+        val expectedImg = ImageIO.read(this::class.java.getResource("test_jiji_expected.png"))
         val outputImg = ImageIO.read(result.previewFile)
 
         (0 until 120).forEach { y ->
             (0 until 320).forEach { x ->
-                val inputPx = inputImg.getRGB(x, y)
+                val expectedPx = expectedImg.getRGB(x, y)
                 val outputPx = outputImg.getRGB(x, y)
                 assertEquals(
-                    inputPx,
+                    expectedPx,
                     outputPx,
-                    "Pixels not equal at $x,$y: input=$inputPx, output=$outputPx"
+                    "Pixels not equal at $x,$y: expected=$expectedPx, output=$outputPx"
                 )
             }
         }
