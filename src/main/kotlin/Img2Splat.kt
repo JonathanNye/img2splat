a0155b3f988db01b31cc651b01020333aa914c06
Jonathan Nye <jonba2@gmail.com>
2022-10-15 16:03:24 -0400

    Add inverted macro/preview output

--- src/main/kotlin/Img2Splat.kt
+++ src/main/kotlin/Img2Splat.kt
@@ -3,7 +3,9 @@ import kotlinx.cli.ArgType
 import java.awt.image.BufferedImage
 import java.io.File
 import java.math.BigDecimal
+import java.text.DecimalFormat
 import javax.imageio.ImageIO
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
@@ -11,15 +13,19 @@ const val WIDTH = 320
 const val HEIGHT = 120
 const val MACRO_FILENAME = "splat_macro.txt"
 const val PREVIEW_FILENAME = "macro_preview.png"
+const val MACRO_FILENAME_INVERTED = "splat_macro_inverted.txt"
+const val PREVIEW_FILENAME_INVERTED = "macro_preview_inverted.png"
 const val PREVIEW_TYPE = "png"
 val DEFAULT_PRESS_DURATION = BigDecimal("0.1")
 val MACRO_START_DELAY = BigDecimal("3.0")
+val MACRO_END_DELAY = BigDecimal("5.0")
 
 enum class Button(val macroToken: String) {
     DOWN("DPAD_DOWN"),
     LEFT("DPAD_LEFT"),
     RIGHT("DPAD_RIGHT"),
     A("A"),
+    B("B"),
     HOME("HOME"),
     MINUS("MINUS"),
 }
@@ -35,9 +41,13 @@ class Command(
     }
 }
 
-class MacroBuilder(private val defaultPressDuration: BigDecimal) {
+class MacroBuilder(
+    private val defaultPressDuration: BigDecimal,
+    private val inverted: Boolean,
+) {
     private val _commands: MutableList<Command> = mutableListOf()
     val commands: List<Command> = _commands
+    private val drawButton = if (inverted) Button.B else Button.A
 
     fun addButtonPress(vararg button: Button, duration: BigDecimal = defaultPressDuration) {
         _commands.add(Command(duration, *button))
@@ -48,24 +58,86 @@ class MacroBuilder(private val defaultPressDuration: BigDecimal) {
         _commands.add(Command(duration))
     }
 
-    fun addRunLengthEncodedLine(encodedLine: RunLengthEncodedLine, duration: BigDecimal = defaultPressDuration) {
+    fun drawImage(img: BufferedImage, repairRows: List<Int>) {
+        var currentDirection: HorizontalDirection = HorizontalDirection.RIGHT
+        var currentX = 0
+
+        for (y in (0 until HEIGHT)) {
+            val currentLineExtent = if (repairRows.contains(y)) {
+                img.rowDrawRange(y, inverted)
+            } else null
+            val nextLineExtent = if (repairRows.contains(y + 1)) {
+                img.rowDrawRange(y + 1, inverted)
+            } else null
+
+            // Only add the commands and swap direction if:
+            // 1. the current line has pixels to draw
+            // 2. the next line has pixels we need to draw, and we should move to the start of the next line
+            if (currentLineExtent != null || nextLineExtent != null) {
+
+                val currentLineRange = when (currentDirection) {
+                    HorizontalDirection.LEFT -> {
+                        val targetX = when {
+                            currentLineExtent != null && nextLineExtent != null -> {
+                                min(currentLineExtent.first, nextLineExtent.first)
+                            }
+                            currentLineExtent != null -> {
+                                currentLineExtent.first
+                            }
+                            else -> { // nextLineExtent != null
+                                nextLineExtent!!.first // compiler not quite clever enough here
+                            }
+                        }
+                        currentX downTo targetX
+                    }
+                    HorizontalDirection.RIGHT -> {
+                        val targetX = when {
+                            currentLineExtent != null && nextLineExtent != null -> {
+                                max(currentLineExtent.last, nextLineExtent.last)
+                            }
+                            currentLineExtent != null -> {
+                                currentLineExtent.last
+                            }
+                            else -> { // nextLineExtent != null
+                                nextLineExtent!!.last // compiler not quite clever enough here
+                            }
+                        }
+                        currentX..targetX
+                    }
+                }
+                val encodedLine = runLengthEncode(img, y, currentLineRange)
+                addRunLengthEncodedLine(encodedLine)
+                currentX = currentLineRange.last
+                currentDirection = when (currentDirection) {
+                    HorizontalDirection.LEFT -> HorizontalDirection.RIGHT
+                    HorizontalDirection.RIGHT -> HorizontalDirection.LEFT
+                }
+            }
+            if (y < HEIGHT - 1) { // Don't go off the bottom edge
+                addButtonPress(Button.DOWN)
+            }
+        }
+    }
+
+    private fun addRunLengthEncodedLine(encodedLine: RunLengthEncodedLine, duration: BigDecimal = defaultPressDuration) {
         val moveButton = encodedLine.direction.moveButton
         encodedLine.segments.forEachIndexed { segmentIdx, segment ->
             val isLastSegment = segmentIdx == encodedLine.segments.size - 1
-            if (segment.isBlack) {
+            val shouldDraw = (segment.isBlack && !inverted) || (!segment.isBlack && inverted)
+            if (shouldDraw) {
                 if (segment.length == 1) {
-                    addButtonPress(Button.A)
+                    addButtonPress(drawButton)
                 } else {
                     (0 until (segment.length - 1)).forEach { idx ->
                         if (idx == 0) {
-                            _commands.add(Command(duration, Button.A)) // A goes down
+                            _commands.add(Command(duration, drawButton)) // draw button goes down
                         }
-                        _commands.add(Command(duration, Button.A, moveButton)) // A + move
+                        _commands.add(Command(duration, drawButton, moveButton)) // draw button + move
                         if (idx < segment.length - 1) {
-                            _commands.add(Command(duration, Button.A))
+                            _commands.add(Command(duration, drawButton)) // draw button held
                         }
                     }
-                    _commands.add(Command(duration)) // neutral
+                    _commands.add(Command(duration)) // neutral, draw button goes up
                 }
             } else {
                 repeat(segment.length - 1) {
@@ -75,6 +147,16 @@ class MacroBuilder(private val defaultPressDuration: BigDecimal) {
             if (!isLastSegment) {
                 addButtonPress(moveButton) // move to next segment
             }
+        }
+    }
+
+    fun writeTo(file: File) {
+        val out = file.printWriter()
+        out.use {
+            commands.forEach { command ->
+                out.println(command)
+            }
+            out.flush()
         }
     }
 }
@@ -120,7 +202,9 @@ class Img2Splat(private val options: Options) {
 
     data class Result(
         val macroFile: File,
-        val previewFile: File
+        val previewFile: File,
+        val invertedMacroFile: File,
+        val invertedPreviewFile: File,
     )
     data class Options(
         val img: BufferedImage,
@@ -171,83 +255,55 @@ class Img2Splat(private val options: Options) {
 
     fun splat(): Result {
 
-        val macro = MacroBuilder(options.pressDuration)
-        var currentDirection: HorizontalDirection = HorizontalDirection.RIGHT
+        val regularMacro = MacroBuilder(options.pressDuration, inverted = false)
+        val invertedMacro = MacroBuilder(options.pressDuration, inverted = true)
 
-        macro.addNeutral(MACRO_START_DELAY)
-        var currentX = 0
-        for (y in (0 until HEIGHT)) {
-            
-            val currentLineExtent = if (options.repairRows.contains(y)) {
-                options.img.rowDrawRange(y)
-            } else null
-            val nextLineExtent = if (options.repairRows.contains(y + 1)) {
-                options.img.rowDrawRange(y + 1)
-            } else null
-
-            // Only add the commands and swap direction if:
-            // 1. the current line has pixels to draw
-            // 2. the next line has pixels we need to draw, and we should move to the start of the next line
-            if (currentLineExtent != null || nextLineExtent != null) {
-
-                val currentLineRange = when (currentDirection) {
-                    HorizontalDirection.LEFT -> {
-                        val targetX = when {
-                            currentLineExtent != null && nextLineExtent != null -> {
-                                min(currentLineExtent.first, nextLineExtent.first)
-                            }
-                            currentLineExtent != null -> {
-                                currentLineExtent.first
-                            }
-                            else -> { // nextLineExtent != null
-                                nextLineExtent!!.first // compiler not quite clever enough here
-                            }
-                        }
-                        currentX downTo targetX
-                    }
-                    HorizontalDirection.RIGHT -> {
-                        val targetX = when {
-                            currentLineExtent != null && nextLineExtent != null -> {
-                                max(currentLineExtent.last, nextLineExtent.last)
-                            }
-                            currentLineExtent != null -> {
-                                currentLineExtent.last
-                            }
-                            else -> { // nextLineExtent != null
-                                nextLineExtent!!.last // compiler not quite clever enough here
-                            }
-                        }
-                        currentX..targetX
-                    }
-                }
-                val encodedLine = runLengthEncode(options.img, y, currentLineRange)
-                macro.addRunLengthEncodedLine(encodedLine)
-                currentX = currentLineRange.last
-                currentDirection = when (currentDirection) {
-                    HorizontalDirection.LEFT -> HorizontalDirection.RIGHT
-                    HorizontalDirection.RIGHT -> HorizontalDirection.LEFT
-                }
-            }
-            if (y < HEIGHT - 1) { // Don't go off the bottom edge
-                macro.addButtonPress(Button.DOWN)
-            }
+        for (macro in arrayOf(regularMacro, invertedMacro)) {
+            macro.addNeutral(MACRO_START_DELAY)
+            macro.drawImage(options.img, options.repairRows)
+            macro.addButtonPress(Button.MINUS)
+            macro.addNeutral(MACRO_END_DELAY)
         }
-        macro.addButtonPress(Button.MINUS)
-        macro.addNeutral(BigDecimal("5.0"))
 
-        val macroFile = File(MACRO_FILENAME)
-        val out = macroFile.printWriter()
-        out.use {
-            macro.commands.forEach { command ->
-                out.println(command)
-            }
-            out.flush()
+        val regularFile = File(MACRO_FILENAME)
+        regularMacro.writeTo(regularFile)
+        val invertedFile = File(MACRO_FILENAME_INVERTED)
+        invertedMacro.writeTo(invertedFile)
+
+        val regularPreviewFile = File(PREVIEW_FILENAME)
+        generatePreview(regularPreviewFile, regularMacro.commands, inverted = false)
+        val invertedPreviewFile = File(PREVIEW_FILENAME_INVERTED)
+        generatePreview(invertedPreviewFile, invertedMacro.commands, inverted = true)
+
+        val regNumCommands = regularMacro.commands.size
+        val invNumCommands = invertedMacro.commands.size
+        val invRecommended = invNumCommands < regNumCommands
+        val recommendedFile = if (invRecommended) {
+            MACRO_FILENAME_INVERTED
+        } else {
+            MACRO_FILENAME
         }
-        val previewFile = generatePreview(macro.commands)
-        println("Generated ${macro.commands.size} operations. Woomy!")
+
+        println("Generated $MACRO_FILENAME with $regNumCommands operations.")
+        println("Generated $MACRO_FILENAME_INVERTED with $invNumCommands operations.")
+
+        val higher = max(regNumCommands, invNumCommands).toDouble()
+        val lower = min(regNumCommands, invNumCommands).toDouble()
+        val percentDecrease = abs(higher - lower) / higher * 100.0
+        if (percentDecrease < 1) {
+            println("They're pretty close, probably just use $MACRO_FILENAME.")
+        } else {
+            val formattedDiff = DecimalFormat("0.0#").format(percentDecrease)
+            println("Recommend using $recommendedFile, which has $formattedDiff% fewer operations.")
+        }
+        println("If you decide to use the inverted macro, don't forget to fill your canvas with black pixels before starting!")
+        println("Woomy!")
+
         return Result(
-            macroFile = macroFile,
-            previewFile = previewFile,
+            macroFile = regularFile,
+            previewFile = regularPreviewFile,
+            invertedMacroFile = invertedFile,
+            invertedPreviewFile = invertedPreviewFile,
         )
     }
 }
@@ -300,17 +356,19 @@ fun BufferedImage.blackPixel(x: Int, y: Int): Boolean {
 
 // returns the x indexes of the outermost drawn pixels, or null if no pixels should be drawn
 // TODO: directional?
-fun BufferedImage.rowDrawRange(y: Int): IntRange? {
+fun BufferedImage.rowDrawRange(y: Int, inverted: Boolean): IntRange? {
     var start: Int? = null
     var end: Int? = null
     for (x in (0 until WIDTH)) {
-        if (blackPixel(x, y)) {
+        val blackPx = blackPixel(x, y)
+        if ((blackPx && !inverted) || (!blackPx && inverted)) {
             start = x
             break
         }
     }
     for (x in ((WIDTH - 1) downTo 0)) {
-        if (blackPixel(x, y)) {
+        val blackPx = blackPixel(x, y)
+        if ((blackPx && !inverted) || (!blackPx && inverted)) {
             end = x
             break
         }
@@ -322,51 +380,68 @@ fun BufferedImage.rowDrawRange(y: Int): IntRange? {
     }
 }
 
-fun generatePreview(commands: List<Command>): File {
+//fun BufferedImage.inversionInfo(): Pair<Int, Boolean> {
+//    val blackPxCount = (0 until HEIGHT).asSequence()
+//        .flatMap { y ->
+//            (0 until WIDTH).asSequence().map { x -> x to y }
+//        }
+//        .map { (x, y) -> blackPixel(x, y) }
+//        .count { it }
+//    return blackPxCount to (blackPxCount > INVERSION_THRESHOLD)
+//}
+
+const val BLUE = 0xFF0000FF.toInt()
+const val WHITE = 0xFFFFFFFF.toInt()
+const val BLACK = 0xFF000000.toInt()
+fun generatePreview(file :File, commands: List<Command>, inverted: Boolean) {
     val img = BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB)
     for (y in (0 until HEIGHT)) {
         for (x in (0 until WIDTH)) {
-            img.setRGB(x, y, 0xFF0000FF.toInt())
+            img.setRGB(x, y, BLUE)
         }
     }
+    val visitedColor = if (inverted) BLACK else WHITE
     var x = 0
     var y = 0
     var aPressed: Boolean
-    img.setRGB(0, 0, 0xFFFFFFFF.toInt())
+    var bPressed: Boolean
+    img.setRGB(0, 0, visitedColor)
     commands.forEach { command ->
+        bPressed = false
         aPressed = false
-        if(command.buttons.contains(Button.A)) {
-            img.setRGB(x, y, 0xFF000000.toInt())
+        if (command.buttons.contains(Button.A)) {
+            img.setRGB(x, y, BLACK)
             aPressed = true
         }
-        if(command.buttons.contains(Button.DOWN)) {
+        if (command.buttons.contains(Button.B)) {
+            img.setRGB(x, y, WHITE)
+            bPressed = true
+        }
+        if (command.buttons.contains(Button.DOWN)) {
             y += 1
-            if (aPressed) {
-                img.setRGB(x, y, 0xFF000000.toInt())
-            } else {
-                img.setRGB(x, y, 0xFFFFFFFF.toInt())
-            }
         }
-        if(command.buttons.contains(Button.LEFT)) {
+        if (command.buttons.contains(Button.LEFT)) {
             x -= 1
-            if (aPressed) {
-                img.setRGB(x, y, 0xFF000000.toInt())
-            } else {
-                img.setRGB(x, y, 0xFFFFFFFF.toInt())
-            }
         }
-        if(command.buttons.contains(Button.RIGHT)) {
+        if (command.buttons.contains(Button.RIGHT)) {
             x += 1
+        }
+        val moved = command.buttons.any {
+            it == Button.DOWN || it == Button.LEFT || it == Button.RIGHT
+        }
+        if (moved) {
             if (aPressed) {
-                img.setRGB(x, y, 0xFF000000.toInt())
-            } else {
-                img.setRGB(x, y, 0xFFFFFFFF.toInt())
+                img.setRGB(x, y, BLACK)
+            }
+            if (bPressed) {
+                img.setRGB(x, y, WHITE)
+            }
+            if (!aPressed && !bPressed) {
+                img.setRGB(x, y, visitedColor)
             }
         }
     }
-    val outFile = File(PREVIEW_FILENAME)
-    ImageIO.write(img, PREVIEW_TYPE, outFile)
-    return outFile
+    ImageIO.write(img, PREVIEW_TYPE, file)
 }
 
 fun String.toRowRange(): IntRange {
